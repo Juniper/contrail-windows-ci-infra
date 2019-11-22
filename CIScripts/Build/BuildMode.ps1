4a475f4215fa404407af1784fc390186cd77f56e
sagarkchitnis <sagarc@juniper.net>
2019-11-22 12:10:31 -0800

    Windows is no longer supported

--- CIScripts/Build/BuildMode.ps1
+++
@@ -1,8 +0,0 @@
-function Resolve-BuildMode {
-    #Helper function to verify in what build configuration/mode
-    #(release/production or debug) Windows compute node components will be built.
-    $IsReleaseMode = (Test-Path Env:BUILD_IN_RELEASE_MODE) -and ("0" -ne $Env:BUILD_IN_RELEASE_MODE)
-    $BuildMode = $(if ($IsReleaseMode) { "production" } else { "debug" })
-
-    return $BuildMode
-}
